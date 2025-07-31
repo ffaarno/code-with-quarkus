2493727c907c598f0716d50660de89e4954c0858
Arno Scholz <arno.scholz@if.de>
2025-07-31 18:06:27 +0200

    add unit and it test, parallel maven task

---
+++ src/test/java/org/acme/MyLivenessCheckIT.java
@@ -0,0 +1,14 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+class MyLivenessCheckIT {
+
+    @Test
+    void call() {
+    }
+}
---
+++ src/test/java/org/acme/MyLivenessCheckTest.java
@@ -0,0 +1,11 @@
+package org.acme;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MyLivenessCheckTest {
+
+    @Test
+    void call() {
+    }
+}
